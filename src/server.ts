da7a2e9126327ec3e9a80bd629d4233fa921aa48
Erik Pablo <nunespablo470@gmail.com>
2025-05-27 09:25:43 -0300

    serverCreate

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-// GET POST PUT DELETE PATCH 
+// GET POST PUT DELETE PATCH
 
 //  http://localhost:3333/hello
 
 app.get('/hello', () => {
-    return 'hello world'
+  return 'hello world'
 })
 
 // Chamando o servidor para escutar na porta 3333
 // o listen vai retorna um Promise, e por isso usamos o then para da um console.log
 
-app.listen({ 
-    port: 3333, 
-}).then(() =>{
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('Server is running')
-})
+  })
